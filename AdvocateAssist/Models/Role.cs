e61ea042bd82b618c13e635ab03a2f36f114f5df
habiba <habibaanjum9799gmail.com>
2025-06-09 02:28:17 +0600

    Class

--- AdvocateAssist/Models/Role.cs
+++ AdvocateAssist/Models/Role.cs
@@ -10,5 +10,27 @@
 
 	public class User
 	{
+		public int UserId { get; set; }
+		public string Username { get; set; }
+		public string Email { get; set; }
+		public string PasswordHash { get; set; }
+		public  int RoleId { get; set; }
+		public virtual Role Role { get; set; }
+		public ICollection<Notification> Notifications { get; set; }
+		public ICollection<TaskItem> TaskItems { get; set; }
+		public ICollection<Appointment> Appointments { get; set; }
+
+	}
+
+	public class Appointment
+	{
+	}
+
+	public class TaskItem
+	{
+	}
+
+	public class Notification
+	{
 	}
 }
